10c508139428440f1737995515675990e0751bc7
Vevila-o <ze0980023@gmail.com>
2026-06-14 14:54:45 +0800

    預覽圖修正

--- static/js/photo.js
+++ static/js/photo.js
@@ -19,16 +19,30 @@ document.getElementById("upload-form").addEventListener("submit",async function(
   console.log(data);
 
   if (data.id){
-    alert("上傳成功")
+    document.getElementById("photo-thumb").src = photoObjectURL;
+    document.getElementById("phase-upload").classList.add("hidden");
+    document.getElementById("phase-record").classList.remove("hidden");
   }
   
 })
+
+let photoObjectURL = null;
 
 // 監聽事件
 document.getElementById("photo-input").addEventListener("change",function(){
 
   // 移除disabled
   document.getElementById("upload-submit").disabled = false;
+
+  // 顯示預覽
+  const file = this.files[0];
+  if (file) {
+    photoObjectURL = URL.createObjectURL(file);
+    const preview = document.getElementById("upload-preview");
+    preview.src = photoObjectURL;
+    preview.classList.add("visible");
+    document.getElementById("drop-content").style.display = "none";
+  }
   
   
 })
